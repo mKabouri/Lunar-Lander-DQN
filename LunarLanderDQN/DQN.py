3fb97ce62d10efd8bc4f08c5d99b849dc66a0dc9
Mohamed Yassine KABOURI <kabouri.yassine@hotmail.com>
2023-10-30 23:50:09 +0100

    Adding sampling and plots

--- LunarLanderDQN/DQN.py
+++ LunarLanderDQN/DQN.py
@@ -3,21 +3,42 @@ import network
 import memoryReplay
 
 import gymnasium as gym
+import numpy as np
+import matplotlib as plt
+import torch
+import torch.optim as optim
+
+env = gym.make("LunarLander-v2", render_mode="human")
 
 class DQN():
-    def __init__(self, nb_outputs, memory_replay_capacity=config.MEMORY_REPLAY_CAPACITY):
+    def __init__(self, nb_outputs, memory_replay_capacity=config.MEMORY_REPLAY_CAPACITY, env=env):
         """
-        
+        Our DQN implementation of Deep Q-Learning algorithm for Lunar Lander
         """
         self.nb_outputs = nb_outputs
-        self.env = gym.make("LunarLander-v2", render_mode="human")
-        observation, info = self.env.reset()
+        self.env = env
+        observation, _ = self.env.reset()
 
         self.replay_memory = memoryReplay.ReplayMemory(capacity=memory_replay_capacity)
-        self.policy_network = network.PolicyNetwork(nb_observations=len(observation), nb_actions=self.env.action_space.n)
 
-    def select_action(self):
-        pass
+        self.policy_network = network.PolicyNetwork(nb_observations=len(observation), nb_actions=self.env.action_space.n).to(config.device)
+        self.target_network = network.PolicyNetwork(nb_observations=len(observation), nb_actions=self.env.action_space.n).to(config.device)
+
+        self.optimizer = optim.SGD(self.policy_network.parameters(), lr=config.LEARNING_RATE)
+
+        self.steps_done = 0
+        self.episode_durations = []
+
+
+    def select_action(self, state):
+        sample = np.random.random()
+        eps_threshold = config.EPS_END + (config.EPS_START-config.EPS_END)*np.exp(-self.steps_done/config.EPS_DECAY)
+        self.steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                return self.policy_network(state).max(1)[1].view(1, 1) # take the largest expected reward
+        else:
+            return torch.tensor([[self.env.action_space.sample()]], device=config.device, dtype=torch.long)
 
     def train(self):
         pass
@@ -25,5 +46,27 @@ class DQN():
     def save_weights(self):
         pass
 
-    def produce_plot(self):
-        pass
+    def produce_plot(self, show_result=False):
+        plt.figure(1)
+        durations_t = torch.tensor(self.episode_durations, dtype=torch.float)
+        if show_result:
+            plt.title('Result')
+        else:
+            plt.clf()
+            plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Duration')
+        plt.plot(durations_t.numpy())
+        # Take 100 episode averages and plot them too
+        if len(durations_t) >= 100:
+            means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(99), means))
+            plt.plot(means.numpy())
+
+        plt.pause(0.001)  # pause a bit so that plots are updated
+        # if is_ipython:
+        #     if not show_result:
+        #         display.display(plt.gcf())
+        #         display.clear_output(wait=True)
+        #     else:
+        #         display.display(plt.gcf())
--- LunarLanderDQN/config.py
+++ LunarLanderDQN/config.py
@@ -1,3 +1,14 @@
-BATCH_SIZE=32
-MEMORY_REPLAY_CAPACITY=320 # To have 10 batches
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+BATCH_SIZE=64
+MEMORY_REPLAY_CAPACITY=1024 # To have 16 batches
 DISCOUNT=0.9
+LEARNING_RATE=1e-3
+#############################
+# For greedy sample selection
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+#############################
